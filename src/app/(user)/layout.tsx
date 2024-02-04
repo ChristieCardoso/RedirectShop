1046382685e4341935a64d36ee5a2c2fe62d353f
ChristieCardoso <christiecardoso@hotmail.com>
2024-02-04 13:52:53 -0300

    🛠️  chore(Update sanity config)

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,19 +1,18 @@
 import type { Metadata } from "next";
-import Navbar from "@/components/Navbar";
-
 import "../../styles/globals.css";
+import Navbar from "@/components/Navbar";
+import "slick-carousel/slick/slick.css";
 
 export const metadata: Metadata = {
   title: "Redirect Shop",
   description:
     "Redirect Shop oferece redirecionamento para sites de afiliados e descontos exclusivos",
 };
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className="font-display">
--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Banner from "@/components/Banner";
+import { client } from "@/lib/sanityClient";
+import { groq } from "next-sanity";
 
-const HomePage = () => {
+export const revalidate = 10;
+const bannerQuery = groq`*[_type == 'banner']{
+  image,
+  _id
+} | order(_createdAt asc)`;
+
+const HomePage = async () => {
+  const banners = await client.fetch(bannerQuery);
   return (
-    <div>HomePage</div>
-  )
-}
+    <main className="text-sm overflow-hidden min-h-screen">
+      <Banner banners={banners} />
+    </main>
+  );
+};
 
-export default HomePage
+export default HomePage;
